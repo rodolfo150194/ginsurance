156cc287e8cf0c820b158e64e5eeae07277d3ad6
rodolfo <rodolfo@code43w.net>
2024-07-16 19:37:52 -0400

    configurando para vercel

--- gi_app/settings.py
+++ gi_app/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-193a@$^6p6l(f5jq%c=esi*nfxp*cq4+ux3cyqj^d9j)%s23+^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1", ".vercel.app"]
 
 
 # Application definition
