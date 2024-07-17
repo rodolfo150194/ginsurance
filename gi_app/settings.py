e96f749577562909b73ab634ba5ed4ec2a83673f
rodolfo <rodolfo@code43w.net>
2024-07-16 23:57:44 -0400

    configurando archivos estaticos

--- gi_app/settings.py
+++ gi_app/settings.py
@@ -121,8 +121,9 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 STATIC_FILES = os.path.join(BASE_DIR / 'static')
-
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 MEDIA_URL = '/media/'
