09ca7cbb43bc7996b3a7c58e03a2e0ea34985564
rodolfo <rodolfo@code43w.net>
2024-07-16 23:19:46 -0400

    configurando archivos estaticos

--- gi_app/settings.py
+++ gi_app/settings.py
@@ -118,11 +118,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATICFILES_DIRS = [
-    os.path.join(PROJECT_DIR, "static"),
-]
+STATIC_URL = 'static/'
 
-STATIC_URL = "/static/"
+STATIC_FILES = os.path.join(BASE_DIR / 'static')
+
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static/'),)
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 MEDIA_URL = "/media/"
 
